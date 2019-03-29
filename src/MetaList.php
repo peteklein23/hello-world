4be7041d72c2da062fcf1b496c4d849d429c7153
Pete Klein <pklein@ackmanndickenson.com>
2019-03-28 19:50:10 -0500

    added test files

---
+++ src/MetaList.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PeteKlein\WPEfficientMeta;
+
+class MetaList
+{
+    public function __construct()
+    {
+    }
+
+    public function test()
+    {
+        echo '<pre>';
+        print_r(['hello']);
+        echo '</pre>';
+    }
+}
