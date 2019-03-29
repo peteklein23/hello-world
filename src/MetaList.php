9e8d40e97a28846d666f2e1abf989ec346964c26
Pete Klein <pklein@ackmanndickenson.com>
2019-03-28 22:13:56 -0500

    Incremental commit

--- src/MetaList.php
+++ src/MetaList.php
@@ -4,14 +4,36 @@ namespace PeteKlein\WPEfficientMeta;
 
 class MetaList
 {
-    public function __construct()
+    public $metaFields = [];
+
+    public function __construct(array $metaFields = [])
+    {
+        foreach ($metaFields as $metaField) {
+            $this->addMetaField($metaField);
+        }
+    }
+
+    public function addMetaField(MetaField $metaField)
+    {
+        $metaList[] = $metaField;
+    }
+
+    public function listValues(array $fieldKeys, array $postIds, bool $indexById = false)
+    {
+        global $wpdb;
+
+        if ($fieldKeys) {
+        
+            // run query and get results
+            $query = "";
+        }
+    }
+
+    public function getFieldKeys()
     {
     }
 
-    public function test()
+    public function setValues(array $fieldValues)
     {
-        echo '<pre>';
-        print_r(['hello']);
-        echo '</pre>';
     }
 }
